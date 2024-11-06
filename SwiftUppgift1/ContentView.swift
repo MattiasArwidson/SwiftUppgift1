eb65a5abe553668496536ea209d136ccac49d26e
Mattias Arwidson <mattias@actionawareness.se>
2024-11-06 14:53:02 +0100

    SwiftUppgift1 all shapes and colors

--- SwiftUppgift1/ContentView.swift
+++ SwiftUppgift1/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack { //MainContainer (Blue)
+            HStack{ // RGYContainer
+                VStack{}//Red
+                    .frame(maxWidth: .infinity, maxHeight: 150)
+                    .background(Color(red: 0.916, green: 0.304, blue: 0.237))
+                VStack{}//Green
+                    .frame(maxWidth: .infinity, maxHeight: 150)
+                    .background(Color(red: 0.389, green: 0.77, blue: 0.398))
+                VStack{}//Yellow
+                    .frame(maxWidth: .infinity, maxHeight: 150)
+                    .background(Color(red: 0.967, green: 0.806, blue: 0.277))
+            }
+            VStack{}//Gray
+                .frame(maxWidth: .infinity, maxHeight: 100)
+                .background(Color(red: 0.749, green: 0.749, blue: 0.749))
+            VStack{}//White
+                .frame(maxWidth: 100, maxHeight: 100)
+                .background(Color.white)
+            VStack{
+                VStack{}// Square (Black)
+                    .frame(maxWidth: 50, maxHeight: 50)
+                    .background(Color.black)
+            }//BottomBar (Red)
+            .frame(maxWidth: .infinity, maxHeight: 100)
+            .background(Color(red: 0.916, green: 0.304, blue: 0.237))
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(red: 0.349, green: 0.77, blue: 0.964))
+        .padding(1)
     }
 }
 
